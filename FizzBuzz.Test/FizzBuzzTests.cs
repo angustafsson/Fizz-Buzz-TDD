903e42affcdf34ac5f226b1029a00bd4e0be6bd5
angustafsson <andreas.g@live.com>
2013-10-29 16:09:57 +0100

    Init Commit
    First commit added fizz buzz tdd practice

---
+++ FizzBuzz.Test/FizzBuzzTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using Product;
+
+namespace FizzBuzz.Test
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [TestCase(1, Result = "1 ")]
+        [TestCase(2, Result = "1 2 ")]
+        [TestCase(3, Result = "1 2 fizz ")]
+        [TestCase(4, Result = "1 2 fizz 4 ")]
+        [TestCase(5, Result = "1 2 fizz 4 buzz ")]
+        [TestCase(15, Result = "1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz ")]
+        public string FizzBuzz_PrintsRightNumber(int number)
+        {
+            return FizzBuzzProvider.Generate(number);
+        }
+    }
+}
---
+++ Product/FizzBuzzProvider.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+
+namespace Product
+{
+    public static class FizzBuzzProvider
+    {
+        public static string Generate(int number)
+        {
+            var str = new StringBuilder();
+            for (var i = 1; i < number + 1; i++)
+            {
+                AddValueToString(i, str);
+            }
+            return str.ToString();
+        }
+
+        private static void AddValueToString(int i, StringBuilder str)
+        {
+            if (IsDivisibleByThree(i) && IsDivisibleByFive(i))
+            {
+                AddFizzBuzz(str);
+            }
+            else if (IsDivisibleByThree(i))
+            {
+                AddFizz(str);
+            }
+            else if (IsDivisibleByFive(i))
+            {
+                AddBuzz(str);
+            }
+            else
+            {
+                AddNumber(str, i);
+            }
+        }
+
+        private static void AddNumber(StringBuilder str, int i)
+        {
+            str.AppendFormat("{0} ", i);
+        }
+
+        private static void AddFizzBuzz(StringBuilder str)
+        {
+            str.Append("fizzbuzz ");
+        }
+
+        private static void AddFizz(StringBuilder str)
+        {
+            str.Append("fizz ");
+        }
+
+        private static void AddBuzz(StringBuilder str)
+        {
+            str.Append("buzz ");
+        }
+
+        private static bool IsDivisibleByThree(int i)
+        {
+            return i%3 == 0;
+        }
+
+        private static bool IsDivisibleByFive(int i)
+        {
+            return i%5 == 0;
+        }
+    }
+}
